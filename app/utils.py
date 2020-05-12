ed542de8a2687f6d01a4d8053d0344da84be76b4
krucan-a <andrzejkrucan@gmail.com>
2020-05-12 20:44:28 +0200

    AK 06-05-2020

---
+++ app/utils.py
@@ -0,0 +1,9 @@
+
+
+
+#funkcja do usuwania znaków formatujacych
+def remove_whitespaces(string):
+    try:
+        return string.replace("\n", ", ").replace("\r", ", ")
+    except AttributeError:
+        pass
